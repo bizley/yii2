fef06d6f5f98921f025e893f154c39d2c5452a0b
Bizley <pawel@positive.codes>
2021-01-23 17:08:16 +0100

    REST try

--- tests/framework/web/SpeedUrlManagerParseUrlTest.php
+++ tests/framework/web/SpeedUrlManagerParseUrlTest.php
@@ -46,4 +46,43 @@ class SpeedUrlManagerParseUrlTest extends UrlManagerParseUrlTest
         var_dump(['parsed' => microtime(true) - $step]);
         $this->assertEquals(['site/index', []], $result);
     }
+
+    public function testRestSpeed()
+    {
+        $rules = [];
+        $charSet = range('a', 'z');
+        foreach ($charSet as $c1) {
+            foreach ($charSet as $c2) {
+                foreach ($charSet as $c3) {
+                    $name = $c1 . $c2 . $c3;
+                    $rules[] = [
+                        'class' => 'yii\rest\UrlRule',
+                        'controller' => $name,
+                    ];
+                }
+            }
+        }
+
+        $cache = new ArrayCache();
+
+        $step = microtime(true);
+        $manager = $this->getUrlManager([
+                                            'rules' => $rules,
+                                            'cache' => $cache,
+                                        ]);
+        var_dump(['init+cache' => microtime(true) - $step]);
+
+
+        $step = microtime(true);
+        $manager = $this->getUrlManager([
+                                            'rules' => $rules,
+                                            'cache' => $cache,
+                                        ]);
+        var_dump(['init' => microtime(true) - $step]);
+
+        $step = microtime(true);
+        $result = $manager->parseRequest($this->getRequest('site/index'));
+        var_dump(['parsed' => microtime(true) - $step]);
+        $this->assertEquals(['site/index', []], $result);
+    }
 }
